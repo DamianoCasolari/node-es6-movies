0526af9848ca2ca0f7390212126912857a00e30d
DamianoCasolari <damianocasolari@gmail.com>
2023-11-08 14:20:47 +0100

    Init commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const filmESerieTV = [
+    {
+      title: "Il Padrino",
+      year: 1972,
+      genre: "Crime, Drama",
+      rating: 9.2,
+      type: "movie"
+    },
+    {
+      title: "Breaking Bad",
+      year: 2008,
+      genre: "Crime, Drama, Thriller",
+      rating: 9.5,
+      type: "tv",
+      seasons: 5
+    },
+    {
+      title: "Inception",
+      year: 2010,
+      genre: "Action, Adventure, Sci-Fi",
+      rating: 8.8,
+      type: "movie"
+    },
+    {
+      title: "Game of Thrones",
+      year: 2011,
+      genre: "Action, Adventure, Drama",
+      rating: 9.3,
+      type: "tv",
+      seasons: 8
+    }
+  ];
+  
+  console.log(filmESerieTV);
+
+  
+  
